39959fa3b2cfe1d2df6e89ec39e68176fbea2eb6
Janderson <janderson.mcast@gmail.com>
2025-12-23 19:43:29 -0300

    criada logica impostSal

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,50 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double rendAnual, rendMensal, rendPrestServ, rendGanhCapit, gastMedc, gastEduc, impostSalario;
+
+		System.out.printf("Renda anual com salário: ");
+		rendAnual = sc.nextDouble();
+		
+		System.out.printf("Renda anual com prestação de serviço: ");
+		rendPrestServ = sc.nextDouble();
+		
+		
+		System.out.println("Renda anual com ganho de capital:");
+		rendGanhCapit = sc.nextDouble();
+		
+		System.out.printf("Gastos médicos: ");
+		gastMedc = sc.nextDouble();
+		
+		System.out.println("Gastos educacionais:");
+		gastEduc = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA ");
+		System.out.println();
+		
+		
+		System.out.println("CONSOLIDADO DE RENDA");
 		
 		
 		
+		rendMensal = rendAnual / 12;
 		
-	double rendAnual;	
+		
+		if(rendMensal < 3000) {
+			impostSalario = 0;
+		}
+		
+		
+		
 		
 
-    System.out.printf("Renda anual com salário: ");
-  
-		
-		
-    		
-    		
-    		
-    		
- 		
-		
 	}
-	
 
 }
-
-
- 
